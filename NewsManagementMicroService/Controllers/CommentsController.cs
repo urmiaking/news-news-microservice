7e907b25af0f5e8e87c0350842bce1f12b2cd2e8
Masoud Khodadadi <masoud.xpress@gmail.com>
2020-06-20 01:00:06 +0430

    Add CommentsController

---
+++ NewsManagementMicroService/Controllers/CommentsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewsManagementMicroService.Data;
+using NewsManagementMicroService.Models;
+
+namespace NewsManagementMicroService.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly NewsManagementMicroServiceContext _context;
+
+        public CommentsController(NewsManagementMicroServiceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Comments/GetComments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comment>>> GetComments()
+        {
+            return await _context.Comments.ToListAsync();
+        }
+
+        // GET: api/Comments/GetComments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Comment>> GetComment(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return comment;
+        }
+
+        // PUT: api/Comments/PutComment/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutComment(int id, Comment comment)
+        {
+            if (id != comment.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(comment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CommentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Comments/PostComment
+        [HttpPost]
+        public async Task<ActionResult<Comment>> PostComment(Comment comment)
+        {
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetComment", new { id = comment.Id }, comment);
+        }
+
+        // DELETE: api/Comments/DeleteComment/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Comment>> DeleteComment(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
+
+            return comment;
+        }
+
+        private bool CommentExists(int id)
+        {
+            return _context.Comments.Any(e => e.Id == id);
+        }
+    }
+}
